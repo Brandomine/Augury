2f21d7cad6d99acd7a8e414b70e2f2a7e0344dcf
Brandomine <brandenricci@hotmail.com>
2015-02-15 13:56:54 -0500

    Brando Commit 8 Removed Multistructure

--- src/main/java/com/nj/augury/init/ModTileEntities.java
+++ src/main/java/com/nj/augury/init/ModTileEntities.java
@@ -1,6 +1,5 @@
 package com.nj.augury.init;
 
-import com.nj.augury.multiblock.MultiblockAlter1;
 import com.nj.augury.references.Reference;
 
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -12,6 +11,5 @@ public class ModTileEntities extends TileEntity{
 	}
 	
 	public static void init(){
-		GameRegistry.registerTileEntity(MultiblockAlter1.class, Reference.MODID);
 	}
 }
