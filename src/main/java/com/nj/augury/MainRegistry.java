c5ed690f8fdf17c26510b2c78b325168a3bf51f0
Brandomine <brandenricci@hotmail.com>
2015-02-14 23:08:25 -0500

    Brando Commit 1
    -mainreg
    -reference

--- src/main/java/com/nj/augury/MainRegistry.java
+++ src/main/java/com/nj/augury/MainRegistry.java
@@ -1,16 +1,15 @@
-package com.example.examplemod;
+package com.nj.augury;
+
+import com.nj.augury.references.Reference;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
-{
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
-    
+@Mod(modid = Reference.MODID, version = Reference.VERSION)
+public class MainRegistry
+{   
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
---
+++ src/main/java/com/nj/augury/references/Reference.java
@@ -0,0 +1,6 @@
+package com.nj.augury.references;
+
+public class Reference {
+	public static final String MODID = "Augury";
+	public static final String VERSION = "1.7.10-0.0.1";
+}
