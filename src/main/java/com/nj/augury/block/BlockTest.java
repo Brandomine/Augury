74381e590aedeb7a4e078fbcf43cf7b9800f58e9
Brandomine <brandenricci@hotmail.com>
2015-02-15 07:56:58 -0500

    Brando Commit 5 Shad-Multiblock Api
    makes forming multiblocks a whole lot easier

---
+++ src/main/java/com/nj/augury/block/BlockTest.java
@@ -0,0 +1,12 @@
+package com.nj.augury.block;
+
+import com.nj.augury.references.Names;
+
+public class BlockTest extends BlockWrapper{
+	public BlockTest()
+    {
+        super();
+        this.setBlockName(Names.Blocks.TEST_BLOCK);
+        this.setBlockTextureName(Names.Blocks.TEST_BLOCK);
+    }
+}
