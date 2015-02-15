67989a0dd79ff1cadc84606cd2010df19f886b36
Brandomine <brandenricci@hotmail.com>
2015-02-15 09:17:39 -0500

    Brando Commit 6 Aspect Names

--- src/main/java/com/nj/augury/references/Names.java
+++ src/main/java/com/nj/augury/references/Names.java
@@ -10,6 +10,55 @@ public class Names {
 	        public static final String ASPECT_WATER = "aspectWater";
 	        public static final String ASPECT_ORDER = "aspectOrder";
 	        public static final String ASPECT_EARTH = "aspectEarth";
+	     //Tier 1
+	        public static final String ASPECT_FROST = "aspectFrost";
+	        public static final String ASPECT_LIGHT = "aspectLight";
+	        public static final String ASPECT_MOTION = "aspectMotion";
+	        public static final String ASPECT_EXCHANGE = "aspectExchange";
+	        public static final String ASPECT_ENERGY = "aspectEnergy";
+	        public static final String ASPECT_WEATHER = "aspectWeather";
+	        public static final String ASPECT_VOID = "aspectVoid";
+	        public static final String ASPECT_POISON = "aspectPoison";
+	        public static final String ASPECT_LIFE = "aspectLife";
+	        public static final String ASPECT_CRYSTAL = "aspectCrystal";
+	     //Tier 2
+	        public static final String ASPECT_BEAST = "aspectBeast";
+	        public static final String ASPECT_HUNGER = "aspectHunger";
+	        public static final String ASPECT_PLANT = "aspectPlant";
+	        public static final String ASPECT_JOURNEY = "aspectJourney";
+	        public static final String ASPECT_SLIME = "aspectSlime";
+	        public static final String ASPECT_METAL = "aspectMetal";
+	        public static final String ASPECT_DEATH = "aspectDeath";
+	        public static final String ASPECT_MAGIC = "aspectMagic";
+	        public static final String ASPECT_HEAL = "aspectHeal";
+	        public static final String ASPECT_DARKNESS = "aspectDarkness";
+	        public static final String ASPECT_TRAP = "aspectTrap";
+	        public static final String ASPECT_FLIGHT = "aspectFlight";
+	      //Tier 3
+	        public static final String ASPECT_ALIEN = "aspectAlien";
+	        public static final String ASPECT_TREE = "aspectTree";
+	        public static final String ASPECT_AURA = "aspectAura";
+	        public static final String ASPECT_FLESH = "aspectFlesh";
+	        public static final String ASPECT_UNDEAD = "aspectUndead";
+	        public static final String ASPECT_SOUL = "aspectSoul";
+	        public static final String ASPECT_TAINT = "aspectTaint";
+	      //Tier 4
+	        public static final String ASPECT_MIND = "aspectMind";
+	        public static final String ASPECT_SENSES = "aspectSenses";
+	      //Tier 5
+	        public static final String ASPECT_MAN = "aspectMan";
+	      //Tier 6
+	        public static final String ASPECT_TOOL = "aspectTool";
+	        public static final String ASPECT_GREED = "aspectGreed";
+	        public static final String ASPECT_CROP = "aspectCrop";
+	        public static final String ASPECT_MINE = "aspectMine";
+	      //Tier 7
+	        public static final String ASPECT_CRAFT = "aspectCraft";
+	        public static final String ASPECT_MACHINE = "aspectMachine";
+	        public static final String ASPECT_HARVEST = "aspectHarvest";
+	        public static final String ASPECT_CLOTH = "aspectCloth";
+	        public static final String ASPECT_WEAPON = "aspectWeapon";
+	        public static final String ASPECT_ARMOR = "aspectArmor";
 	    }
 	 public static final class Blocks
 	 	{
