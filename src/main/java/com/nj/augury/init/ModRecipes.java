a5efc24fff7992044f756519c0e9f592ba6a1178
Brandomine <brandenricci@hotmail.com>
2015-02-22 20:14:33 -0500

    Brando Commit 22 Recipes

--- src/main/java/com/nj/augury/init/ModRecipes.java
+++ src/main/java/com/nj/augury/init/ModRecipes.java
@@ -8,6 +8,17 @@ import cpw.mods.fml.common.registry.GameRegistry;
 
 public class ModRecipes {
 	public static void init(){
-		ThermalExpansionHelper.addSmelterRecipe(1000, (new ItemStack(ModItems.aspectFire)), (new ItemStack(ModItems.aspectEntropy)), (new ItemStack(ModItems.aspectFrost)), null, 0);;
+		//Tier 1
+		ThermalExpansionHelper.addSmelterRecipe(5000, (new ItemStack(ModItems.aspectFire)), (new ItemStack(ModItems.aspectEntropy)), (new ItemStack(ModItems.aspectFrost)), null, 0);
+		ThermalExpansionHelper.addSmelterRecipe(5000, (new ItemStack(ModItems.aspectAir)), (new ItemStack(ModItems.aspectFire)), (new ItemStack(ModItems.aspectLight)), null, 0);
+		ThermalExpansionHelper.addSmelterRecipe(5000, (new ItemStack(ModItems.aspectAir)), (new ItemStack(ModItems.aspectOrder)), (new ItemStack(ModItems.aspectMotion)), null, 0);
+		ThermalExpansionHelper.addSmelterRecipe(5000, (new ItemStack(ModItems.aspectOrder)), (new ItemStack(ModItems.aspectEntropy)), (new ItemStack(ModItems.aspectExchange)), null, 0);
+		ThermalExpansionHelper.addSmelterRecipe(5000, (new ItemStack(ModItems.aspectFire)), (new ItemStack(ModItems.aspectOrder)), (new ItemStack(ModItems.aspectEnergy)), null, 0);
+		ThermalExpansionHelper.addSmelterRecipe(5000, (new ItemStack(ModItems.aspectAir)), (new ItemStack(ModItems.aspectWater)), (new ItemStack(ModItems.aspectWeather)), null, 0);
+		ThermalExpansionHelper.addSmelterRecipe(5000, (new ItemStack(ModItems.aspectAir)), (new ItemStack(ModItems.aspectEntropy)), (new ItemStack(ModItems.aspectVoid)), null, 0);
+		ThermalExpansionHelper.addSmelterRecipe(5000, (new ItemStack(ModItems.aspectWater)), (new ItemStack(ModItems.aspectEntropy)), (new ItemStack(ModItems.aspectPoison)), null, 0);
+		ThermalExpansionHelper.addSmelterRecipe(5000, (new ItemStack(ModItems.aspectWater)), (new ItemStack(ModItems.aspectEarth)), (new ItemStack(ModItems.aspectLife)), null, 0);
+		ThermalExpansionHelper.addSmelterRecipe(5000, (new ItemStack(ModItems.aspectOrder)), (new ItemStack(ModItems.aspectEarth)), (new ItemStack(ModItems.aspectCrystal)), null, 0);
+		//Tier 2
 	}
 }
